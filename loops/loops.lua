db2a7cf2fd380a419bd2834896fffbe8ff49b9af
myferr <contactme.myfer@protonmail.com>
2025-06-20 20:31:26 -0500

    Add loops

---
+++ loops/loops.lua
@@ -0,0 +1,9 @@
+print("Enter input.")
+userNumericalValue = io.read("n")
+print("End value.")
+userNumericalEndValue = io.read("n")
+
+while userNumericalValue < userNumericalEndValue do
+    userNumericalValue = userNumericalValue + 1
+    print(userNumericalValue)
+end
